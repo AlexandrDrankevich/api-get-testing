cabf67d8bd8a035d102ac657a60f3f798c3aa773
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-12 14:29:33 +0300

    done

--- src/test/java/GetWebCamsOceanDrivers.java
+++ src/test/java/GetWebCamsOceanDrivers.java
@@ -2,10 +2,8 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -17,18 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GetWebCamsOceanDrivers {
     private final String NAME = "Cala Agulla";
-    private final String KEY="name";
+    private final String KEY = "name";
 
     @Test
-    public void testGetWebCams() throws IOException, ParseException {
+    public void testGetWebCams() throws IOException {
         HttpGet request = new HttpGet("http://api.oceandrivers.com/v1.0/getWebCams/");
         CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
         String entity = EntityUtils.toString(response.getEntity());
-        JSONParser parser = new JSONParser();
-        JSONArray jsonArray = (JSONArray) parser.parse(entity);
+        JSONArray jsonArray = new JSONArray(entity);
         List<String> namesList = new ArrayList<>();
         for (Object jsonObject : jsonArray) {
-            namesList.add(((JSONObject) jsonObject).get(KEY).toString());
+            namesList.add(((JSONObject) jsonObject).getString(KEY));
         }
         assertEquals(200, response.getStatusLine().getStatusCode());
         assertTrue(namesList.contains(NAME));
